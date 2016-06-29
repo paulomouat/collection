252f1e70cc4adc2392527293114195fc59726528
Paulo Mouat <paulo.mouat@gmail.com>
2016-06-29 18:11:00 -0400

    Testing script

--- update.sh
+++ update.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-cp *.html ../gh-pages
+#cp *.html ../gh-pages
+git add -A
+git commit
+git push
 cp ../../contempworks/generated/*.html ../gh-pages/contempworks
+cd ../gh-pages
+git add -A
+git merge -m "Merged latest from master" origin/master
+git push
