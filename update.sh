3735abb309bb183de62ba835c9d51956b7d1a919
Paulo Mouat <paulo.mouat@gmail.com>
2016-07-03 10:04:45 -0400

    master and gh-pages are now on the same physical folder.

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #cp *.html ../gh-pages
-cp ../../contempworks/generated/*.html ../master/contempworks
+git checkout master
+cp ../../contempworks/generated/*.html contempworks
 git add -A
 git commit
 git push
-cd ../gh-pages
-git add -A
+git checkout gh-pages
 git merge -m "Merged latest from master" origin/master
 git push
